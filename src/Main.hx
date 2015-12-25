ddbed2c55e883027c4c5005a1cc3a12df29f0837
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2015-12-25 18:35:15 +0700

    Wait until stage size is correct and stable before starting anything

--- src/Main.hx
+++ src/Main.hx
@@ -1,26 +1,41 @@
 package ;
 
+import flash.Lib;
 import flash.display.StageAlign;
 import flash.display.StageScaleMode;
-import flash.display.StageQuality;
-import flash.Lib;
+import flash.events.Event;
 import flash.ui.Multitouch;
 import flash.ui.MultitouchInputMode;
 
+class Main 
+{
+    private static var stage = Lib.current.stage;
 
-class Main {
-
-    public static function main() {
+    private static function initialize()
+    {
+        //
+        // TODO: BEGIN HERE
+        //
+    }
+    
+    private static function main() 
+    {
         Multitouch.inputMode = MultitouchInputMode.TOUCH_POINT;
 
-        var stage = Lib.current.stage;
         stage.scaleMode = StageScaleMode.NO_SCALE;
         stage.align = StageAlign.TOP_LEFT;
-        stage.quality = StageQuality.LOW;
+        stage.addEventListener(Event.RESIZE, onResize);
+    }
 
-        //
-        // Begin here
-        //
+    private static function onResize(e:Event)
+    {
+        var correctSizeReported = cast(stage.fullScreenWidth,  Int) == stage.stageWidth 
+                               && cast(stage.fullScreenHeight, Int) == stage.stageHeight;
+
+        if (correctSizeReported) {
+            stage.removeEventListener(Event.RESIZE, onResize);
+            initialize();
+        }
     }
 
 }
